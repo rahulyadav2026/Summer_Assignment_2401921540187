be4a313e60d743471073b7f60f8a1e989696128a
Rahul Yadav <rahulyadav81827@gmail.com>
2026-06-03 15:43:42 +0530

    Create MoveZeroes.cpp

---
+++ week1/day3/MoveZeroes.cpp
@@ -0,0 +1,16 @@
+lass Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        
+        int m=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[m]);
+                m++;
+                
+            }
+        }
+         
+    }
+};
